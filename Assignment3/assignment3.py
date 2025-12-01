94cfb99262c5acb9c323247b024ccb6c3df33264
emmatottie <emma.tottie@hotmail.se>
2025-12-01 16:01:08 +0100

    Final touch

--- Assignment3/assignment3.py
+++ Assignment3/assignment3.py
@@ -16,54 +16,54 @@ fb = 2000
      
 # -------------- TASK 1 -------------- #
 
-sd.play(x_k, fs, blocking=True) # f = 10000 Hz
-#sd.play(x_k, fs2, blocking=True) # f = 1100 Hz
+# sd.play(x_k, fs, blocking=True) # f = 10000 Hz
+# #sd.play(x_k, fs2, blocking=True) # f = 1100 Hz
 
-# Plot x(t), x[k]
-fig, ax = plt.subplots()
-ax.stem(tk, x_k, label="x[k], discrete")
-ax.plot(tk, x_k, label="x(t), continuous")
-ax.set_title("Task 1")
-ax.set_xlabel('t (s)')
-ax.set_ylabel('x(t)')
-ax.legend()
-ax.set_xlim(0, 0.003) 
-ax.set_ylim(-1.1, 1.1)
-ax.grid()
-plt.show()
+# # Plot x(t), x[k]
+# fig, ax = plt.subplots()
+# ax.stem(tk, x_k, label="x[k], discrete")
+# ax.plot(tk, x_k, label="x(t), continuous")
+# ax.set_title("Task 1")
+# ax.set_xlabel('t (s)')
+# ax.set_ylabel('x(t)')
+# ax.legend()
+# ax.set_xlim(0, 0.003) 
+# ax.set_ylim(-1.1, 1.1)
+# ax.grid()
+# plt.show()
 
-# -------------- TASK 2 -------------- #
-tk2 = np.arange(T0, T1, 1/2000)
-x_t2 = np.cos(w*np.pi*tk2)
-sd.play(x_t2, fb, blocking=True) 
+# # -------------- TASK 2 -------------- #
+# tk2 = np.arange(T0, T1, 1/2000)
+# x_t2 = np.cos(w*np.pi*tk2)
+# sd.play(x_t2, fb, blocking=True) 
 
-fig, ax = plt.subplots()
-ax.stem(tk2, x_t2, label="x[k], discrete")
-ax.plot(tk2, x_t2, label="x(t), continuous")
-ax.set_title("Task 1")
-ax.set_xlabel('t (s)')
-ax.set_ylabel('x(t)')
-ax.legend()
-ax.set_xlim(0, 0.003) 
-ax.set_ylim(-1.1, 1.1)
-ax.grid()
-plt.show()
+# fig, ax = plt.subplots()
+# ax.stem(tk2, x_t2, label="x[k], discrete")
+# ax.plot(tk2, x_t2, label="x(t), continuous")
+# ax.set_title("Task 1")
+# ax.set_xlabel('t (s)')
+# ax.set_ylabel('x(t)')
+# ax.legend()
+# ax.set_xlim(0, 0.003) 
+# ax.set_ylim(-1.1, 1.1)
+# ax.grid()
+# plt.show()
 
-y_t = np.sin(w*np.pi*tk)
+# y_t = np.sin(w*np.pi*tk)
 
-sd.play(y_t, fs, blocking=True) # låter samma som 1a) bara lite skillnad i phase
+# sd.play(y_t, fs, blocking=True) # låter samma som 1a) bara lite skillnad i phase
 
-fig, ax = plt.subplots()
-ax.stem(tk, y_t, label="y[k], discrete")
-ax.plot(tk, y_t, label="y(t), continuous")
-ax.set_title("Task 2")
-ax.set_xlabel('t (s)')
-ax.set_ylabel('y(t)')
-ax.legend()
-ax.set_xlim(0, 0.003) 
-ax.set_ylim(-1.1, 1.1)
-ax.grid()
-plt.show()
+# fig, ax = plt.subplots()
+# ax.stem(tk, y_t, label="y[k], discrete")
+# ax.plot(tk, y_t, label="y(t), continuous")
+# ax.set_title("Task 2")
+# ax.set_xlabel('t (s)')
+# ax.set_ylabel('y(t)')
+# ax.legend()
+# ax.set_xlim(0, 0.003) 
+# ax.set_ylim(-1.1, 1.1)
+# ax.grid()
+# plt.show()
 
 # -------------- TASK 3 -------------- #
 T = 10
@@ -74,7 +74,7 @@ x = sd.rec(int(K), fs3, channels=1, blocking=True)
 print("Recording shape:", x.shape)
 
 N = 4
-wn = 3400 
+wn = 3400 * 2*np.pi
 z, p, k = signal.butter(N, wn, analog=True, output='zpk')
 
 H = signal.ZerosPolesGain(z, p, k)
@@ -82,11 +82,11 @@ H = signal.ZerosPolesGain(z, p, k)
 w, magnitude, phase = signal.bode(H, n=1000)
 
 magnitude = 10**(magnitude/20)
-phase = phase * (np.pi/180)
-w = w/2*np.pi
+#phase = phase * (np.pi/180)
+f_hz = w/(2*np.pi) 
 
 fig, ax = plt.subplots()
-ax.plot(w, magnitude, label="Magnitude")
+ax.plot(f_hz, magnitude, label="Magnitude")
 ax.set_title("Task 3, Fourth-Order Butterworth Filter Amplitude Response")
 ax.set_xlabel('f (Hz)')
 ax.set_ylabel('Amplitude Gain, |H(f)|')
